30ee4397ecbc73588217fd61a17665c9e65528ea
thbd <thiagobalbino91@gmail.com>
2022-07-19 15:55:41 -0300

    add class util

---
+++ src/main/java/net/codejava/customer/v2/CommonUtil.java
@@ -0,0 +1,13 @@
+package net.codejava.customer.v2;
+
+public class CommonUtil {
+	
+	public static String getTeste1() {
+		return "Teste v1";
+	}
+	
+	public static String getTeste2() {
+		return "Teste V2";
+	}
+
+}
