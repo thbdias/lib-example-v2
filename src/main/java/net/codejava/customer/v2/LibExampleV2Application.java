14c4ec269a9e022a184ce50258d62f4aff17e97c
thbd <thiagobalbino91@gmail.com>
2022-07-19 14:01:36 -0300

    initial commit

---
+++ src/main/java/net/codejava/customer/v2/LibExampleV2Application.java
@@ -0,0 +1,13 @@
+package net.codejava.customer.v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibExampleV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibExampleV2Application.class, args);
+	}
+
+}
